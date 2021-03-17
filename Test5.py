81e9c507a2a475e230c589ec1e2eee243e8938c9
Raventhal <78174831+Raventhal@users.noreply.github.com>
2021-03-17 13:24:12 -0700

    Add files via upload

---
+++ Test5.py
@@ -0,0 +1,8 @@
+line = input()
+while True:
+ 
+    if line == "Exit":
+        break
+    else:
+        print(line)
+print("Goodbye")
---
+++ def demo(x).py
@@ -0,0 +1,9 @@
+def animals(sound):
+    if sound == "dog":
+        return "bark"
+    elif sound == "cat":
+        return "meow"
+    else:
+        return "Whatcha talkin' bout Willis!"
+
+print(animals("frog"))
