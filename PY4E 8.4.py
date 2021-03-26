a5e8eeb9b5137a84c7f74733967ef23f0ba784c1
Raventhal <78174831+Raventhal@users.noreply.github.com>
2021-03-26 00:31:09 -0400

    Create PY4E 8.4.py

---
+++ PY4E 8.4.py
@@ -0,0 +1,10 @@
+fname = input("Enter file name: ")
+fh = open(fname)
+blanklist= list()
+for filetxt in fh:
+    list_of_words=filetxt.split()
+    for words in list_of_words:
+        if words not in blanklist:
+            blanklist.append(word)
+nowfilledlist= blanklist 
+print(sorted(nowfilledlist))
